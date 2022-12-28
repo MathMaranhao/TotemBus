1af73613cd9706e0a0d24de4d000d72085967a91
gvpp <67529131+gvpp@users.noreply.github.com>
2022-12-27 21:01:23 -0300

    Add files via upload
    Atualização da função de exclusão e alteração na interface

---
+++ TotemBus/test/controller/ControllerItemItinerarioTest.java
@@ -0,0 +1,33 @@
+
+package controller;
+
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+import model.ModelItemItinerario;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+public class ControllerItemItinerarioTest {
+    
+    public ControllerItemItinerarioTest() {
+    }
+
+    @Test
+    public void testCadastrarItemItinerarioRepetido() {
+        
+        System.out.println("cadastrarItemItinerarioController");
+        ControllerItemItinerario controller = new ControllerItemItinerario();
+        List<ModelItemItinerario> listaItensCadastrados = new ArrayList<>();
+        listaItensCadastrados = controller.getListaItensItineraioController();
+        
+        ModelItemItinerario itemItinerario = listaItensCadastrados.get(0);
+        
+        assertEquals(false, controller.cadastrarItemItinerarioController(itemItinerario));
+        
+    }
+
+    
+    
+}
